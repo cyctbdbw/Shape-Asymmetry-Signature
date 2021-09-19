b2bbf1b85211a794c7ac0b99443a4d452633fb55
Yu-Chi Chen <48049448+cyctbdbw@users.noreply.github.com>
2021-09-19 22:49:47 +1000

    Create EF_visualiztion.m

---
+++ EF_visualiztion.m
@@ -0,0 +1,11 @@
+%%This script visualiza the scale of "individual" eigenfunction
+figure;
+EF_no=2;%This is the eigenfunction numer for visualization. You can change to another number
+patch(leftSurface,'FaceVertexCData',eigenFunctionsL(:,EF_no),'EdgeColor','none','FaceColor','interp');
+axis off;
+axis image;
+hold on 
+patch(rightSurface,'FaceVertexCData',eigenFunctionsR(:,EF_no),'EdgeColor','none','FaceColor','interp');
+
+view([0,90]); %view([270,0]) for left himisphere only; view([90 0]) for RH only
+set(gcf,'color','w');
