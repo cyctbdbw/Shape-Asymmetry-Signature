401bdcdd55a91d2a9ffdb1b8dc59921937bd806f
Yu-Chi Chen <48049448+cyctbdbw@users.noreply.github.com>
2021-11-28 17:04:55 +1100

    Create CCA_loading_figure.m

---
+++ cognition_analysis/CCA_loading_figure.m
@@ -0,0 +1,24 @@
+[r_sas,~]=corr(U(:,1),sas_mean);
+figure('color','white');
+b=bar(r_sas,'b');
+b.FaceColor = 'flat';
+un_sig_idx=find(padj_sas>=0.05);
+
+for id=1:length(un_sig_idx)
+    b.CData(un_sig_idx(id),:) = [0.85 0.85 0.85];
+end
+
+hold on
+
+er = errorbar([1:length(r_sas)],r_sas,se_sas,'LineWidth',1,'CapSize',20); 
+%se_sas is from the bootstrapping
+
+er.LineStyle = 'none';
+er.Color = [0 0 0];
+set(gca,'fontSize',20,'box','on')
+a=padj_sas';
+xlabel('Eigen-group')
+ylabel('Loading')
+yticks([-1:0.2:1])
+ylim([-1 1])
+exportgraphics(gcf,'SAS_CCA_loading.jpg','Resolution',900)
