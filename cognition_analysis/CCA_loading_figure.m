bbab6fefd9d6ce6e9bfa5c6a226b4ab8557c32aa
Yu-Chi Chen <48049448+cyctbdbw@users.noreply.github.com>
2021-12-15 12:08:11 +1100

    Update CCA_loading_figure.m

--- cognition_analysis/CCA_loading_figure.m
+++ cognition_analysis/CCA_loading_figure.m
@@ -1,7 +1,7 @@
 [r_sas,~]=corr(U(:,1),sas_mean); % U is the SAS canonical variate; 
 % sas_mean is the mean of SAS across each eigen-group
 figure('color','white');
-b=bar(r_sas,'b');
+b=barh(r_sasCCA,'FaceColor',[0.6350, 0.0780, 0.1840],'EdgeColor','none');
 b.FaceColor = 'flat';
 un_sig_idx=find(padj_sas>=0.05); 
 % padj_sas is FDR corrected p value of the CCA loading, 
@@ -13,15 +13,14 @@ end
 
 hold on
 
-er = errorbar([1:length(r_sas)],r_sas,se_sas,'LineWidth',1,'CapSize',20); 
+er = errorbar(r_sas,[1:11],se_sas,'horizontal','LineWidth',1,'CapSize',14);
 %se_sas is from the bootstrapping
-
 er.LineStyle = 'none';
 er.Color = [0 0 0];
+
 set(gca,'fontSize',20,'box','on')
-a=padj_sas';
 xlabel('Eigen-group')
-ylabel('Loading')
+ylabel('Canonical Loading')
 yticks([-1:0.2:1])
 ylim([-1 1])
 exportgraphics(gcf,'SAS_CCA_loading.jpg','Resolution',900)
